c39973aab3ac2a202177e3835ea1fa27db0b4d0b
Aqib Ali <aqibhalepoto78@gmail.com>
2025-02-06 13:28:39 +0000

    Basics Cleared

---
+++ basics/01_printingHelloWorld.js
@@ -0,0 +1,9 @@
+
+//javascript is a dynamic language and not static
+//To print something we use , console.log
+
+console.log("hello world");
+
+//to print in a table format we use console.table
+
+console.table(["row : 1" , "row : 2" , "row : 3"])
---
+++ basics/02_variables.js
@@ -0,0 +1,25 @@
+
+//variables
+
+/*
+variables in javascript can be defined using var,let and const
+
+const cannot be changed
+
+var and let can be changed but var is not used now a days because of issues faced by using it
+->no concept of scope was being used..
+
+It is better and advisible to use let than var
+
+*/
+
+const gpa = 3.25;
+let age = 19;
+let nameOfStudent = "Aqib";
+favoriteBook = "Stranger";
+
+
+console.table([nameOfStudent, gpa, age, favoriteBook]);
+
+
+//if a value is not initallized, then it is assigned as undefined
